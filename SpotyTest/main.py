6cdfb9eaa451c65ee7a6b08ce790b42bbd2c5a52
JUCA12 <jdac_06@hotmail.com>
2019-10-31 14:01:42 -0600

    Archivos para development

---
+++ SpotyTest/main.py
@@ -0,0 +1,12 @@
+import Track
+from SPYClass import APISFY
+import spotipy
+
+def Search(apisfy):
+    song = input("Como se llama la rola: ")
+    a
+
+def main():
+    apisfy = APISFY()
+if __name__ == '__main__':
+    main()
